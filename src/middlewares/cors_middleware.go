bd49e92c9913eda8130a64c2e7d604565eb7ac8a
Ahmad Saubani <banz.cozy@gmail.com>
2025-07-26 09:55:35 +0700

    perbaikan cors

--- src/middlewares/cors_middleware.go
+++ src/middlewares/cors_middleware.go
@@ -1,32 +1,43 @@
 package middlewares
 
 import (
-	"net/http"
-	"strings"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func CorsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		origin := c.GetHeader("Origin")
-		if origin != "" {
-			if strings.HasPrefix(origin, "http://localhost") ||
-				strings.HasPrefix(origin, "http://127.0.0.1") ||
-				strings.HasSuffix(origin, ".ahmadsaubani.com") {
-				c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-				c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
-				c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
-				c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-			}
-		}
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusOK)
-			return
-		}
-
-		c.Next()
-
-	}
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost", "http://127.0.0.1", "https://api-go.ahmadsaubani.com", "https://ahmadsaubani.com", "https://www.ahmadsaubani.com"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
 }
+
+// func CorsMiddleware() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		origin := c.GetHeader("Origin")
+// 		if origin != "" {
+// 			if strings.HasPrefix(origin, "http://localhost") ||
+// 				strings.HasPrefix(origin, "http://127.0.0.1") ||
+// 				strings.HasSuffix(origin, ".ahmadsaubani.com") {
+// 				c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+// 				c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+// 				c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+// 				c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+// 			}
+// 		}
+
+// 		if c.Request.Method == "OPTIONS" {
+// 			c.AbortWithStatus(http.StatusOK)
+// 			return
+// 		}
+
+// 		c.Next()
+
+// 	}
+// }
