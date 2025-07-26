ee34df64b7dc8badbf54cfeb0c2a5b1d7dc1d2fb
Ahmad Saubani <banz.cozy@gmail.com>
2025-07-26 10:26:33 +0700

    testing cors

--- src/routes/route.go
+++ src/routes/route.go
@@ -6,9 +6,7 @@ import (
 	"news-go/src/controllers/api/v1/article_controllers"
 	"news-go/src/repositories/article_repositories"
 	"news-go/src/services/article_services"
-	"time"
 
-	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -18,21 +16,6 @@ func API(db *database.DBConnection, ginEngine *gin.Engine) *gin.Engine {
 	articleService := article_services.NewArticleService(articleRepository)
 
 	// ginEngine.Use(middlewares.CorsMiddleware())
-
-	ginEngine.Use(cors.New(cors.Config{
-		AllowOrigins: []string{
-			"http://localhost",
-			"http://127.0.0.1",
-			"https://api-go.ahmadsaubani.com",
-			"https://ahmadsaubani.com",
-			"https://www.ahmadsaubani.com",
-		},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		MaxAge:           12 * time.Hour,
-	}))
 
 	v1 := ginEngine.Group("/api/v1")
 	{
